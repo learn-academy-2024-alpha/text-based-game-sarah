22200e89bc60dbfcf8fa24b45036de3fc8101804
Sarah Proctor <sproctor950@gmail.com>
2024-03-07 09:27:35 -0800

    adds text based game options

--- challenge.rb
+++ challenge.rb
@@ -1,1 +1,16 @@
 # Text-based Game Challenge
+
+puts "enter your name"
+user_name = gets
+puts "hi #{user_name}"
+
+options = [2, 3, 4, 5]
+puts "what do you want to do next?"
+gets
+puts options.sample
+
+"hi there, do you want to go on an adventure?".each_char do |char|
+  putc char
+  sleep 0.05
+end
+puts "\n"
